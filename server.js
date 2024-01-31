aef218e318f2cada46a63d4f932a8155d6549b34
yuanhaomichael <yd2473@columbia.edu>
2024-01-31 12:11:41 -0800

    prxy

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const app = express();
 app.use('/', createProxyMiddleware({
   target: 'https://michael-yuanhao-deng.vercel.app/',
   changeOrigin: true,
-  pathRewrite: {
-    '^/': '/posts/My%20Story', // Rewrite root to the specific path
-  },
+  // pathRewrite: {
+  //   '^/': '/posts/My%20Story', // Rewrite root to the specific path
+  // },
 }));
 
 const PORT = process.env.PORT || 3002; // Use the PORT environment variable provided by Heroku or default to 3002
